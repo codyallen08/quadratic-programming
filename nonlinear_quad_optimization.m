964397e0dcf004c2bafbd33cae7920a051a4c3c3
Cody Allen <cwallen08@gmail.com>
2019-08-08 12:05:42 -0700

    added comments to both files

--- nonlinear_quad_optimization.m
+++ nonlinear_quad_optimization.m
@@ -1,16 +1,27 @@
+% example quadratic programming problem solution
+% cody allen 8/8/19
+
 clear
 clc
-
+% set options for minimizer: use sequential quadratic programming 
+% need optimization toolbox
 options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 
+% define objectiv
 fun = @(x) -0.001*x(1)^2 +2*x(1) -0.002*x(2)^2 - 3*x(2) -0.004*x(3)^2 + x(3) + 5;
+
+% initial starting point (make sure this is a feasible point)
 x0 = [0.33,0.33,0.33];
-lb = zeros(3,1);
-ub = ones(size(lb));
-Aeq = ones(1,3);
+
+% add constraints
+% lower and upper bounds
+lb = zeros(3,1);  
+ub = ones(size(lb)); 
+
+% equality constraint
+Aeq = ones(1,3); 
 beq = 1;
 
+% minimize
 [x,fval,exitflag,output] = fmincon(fun,x0,[],[],Aeq,beq,lb,ub,[],options)
-xx = [0;1;0];
 
-fun(x)
